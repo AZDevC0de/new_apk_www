58c0a3818dff5f7f0a282f52fe216aa53b1c1699
zelek684 <73654594+zelek684@users.noreply.github.com>
2023-12-08 15:43:26 +0100

    change file test_models

--- mysite1/polls1/tests/test_models.py
+++ mysite1/polls1/tests/test_models.py
@@ -1,7 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-from ..models import Person, Team
+from ..models import Person, Team, Stanowisko
+from ..models import Osoba
 
+class OsobaModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        #  wymagane obiekty dla relacji ForeignKey
+        wlasciciel = User.objects.create_user(username='testuser', password='testpassword123')
+        stanowisko = Stanowisko.objects.create(nazwa='Developer', opis='Tworzy oprogramowanie.')
+        team = Team.objects.create(name='TeamX', country='X')
+
+        # tworze obiekt Osoba z powiązanymi obiektami
+        Osoba.objects.create(
+            imie='Jan',
+            nazwisko='Kowalski',
+            plec=2,
+            stanowisko=stanowisko,
+            team=team,
+            wlasciciel=wlasciciel
+        )
+
+    def test_imie_label(self):
+        # Pobieram\osobę, którą utworzyliśmy w setUpTestData
+        osoba = Osoba.objects.get(id=1)
+        field_label = osoba._meta.get_field('imie').verbose_name
+        self.assertEqual(field_label, 'imie')
+
+    def test_nazwisko_max_length(self):
+        osoba = Osoba.objects.get(id=1)
+        max_length = osoba._meta.get_field('nazwisko').max_length
+        self.assertEqual(max_length, 70)
 
 # class PersonModelTest(TestCase):
 #     @classmethod
@@ -17,17 +48,17 @@ from ..models import Person, Team
 #         person = Person.objects.get(id=1)
 #         max_length = person._meta.get_field('name').max_length
 #         self.assertEqual(max_length, 60)
-class PersonModelTest(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        Person.objects.create(name='Jan', shirt_size='L')
-        Person.objects.create(name='Anna', shirt_size='M')
-
-    def test_person_ids(self):
-        person1 = Person.objects.get(name='Jan')
-        person2 = Person.objects.get(name='Anna')
-        self.assertNotEqual(person1.id, person2.id)
+# class PersonModelTest(TestCase):
+#
+#     @classmethod
+#     def setUpTestData(cls):
+#         Person.objects.create(name='Jan', shirt_size='L')
+#         Person.objects.create(name='Anna', shirt_size='M')
+#
+#     def test_person_ids(self):
+#         person1 = Person.objects.get(name='Jan')
+#         person2 = Person.objects.get(name='Anna')
+#         self.assertNotEqual(person1.id, person2.id)
 
 class TeamModelTest(TestCase):
 
